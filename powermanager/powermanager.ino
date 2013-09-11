d7e2d0677657f2cb8ef30194f75ba76094b6ed6c
kuba <kuba@kucharski.it>
2013-09-11 21:37:45 +0200

    engine init :)

---
+++ powermanager/powermanager.ino
@@ -0,0 +1,190 @@
+// Kuba Kucharski
+// 2013
+
+#include <SerialCommand.h>
+
+#define arduinoLED 13   // Dioda podpięta do pinu 13
+#define freqPIN 6
+
+SerialCommand sCmd;     // obiekt komunikacji Serial
+
+void setup() {
+  pinMode(arduinoLED, OUTPUT);      // Ustawiam PIN jako wyjscie analogowe
+  digitalWrite(arduinoLED, LOW);    // Dioda wylaczona
+  pinMode(freqPIN, OUTPUT);  
+  
+
+  Serial.begin(9600);
+
+  // Setup callbacks for SerialCommand commands
+  sCmd.addCommand("ON",    LED_on);          // Wlacz
+  sCmd.addCommand("OFF",   LED_off);         // Wylacz
+  sCmd.addCommand("HELLO", sayHello);        // Echo
+  sCmd.addCommand("P",     processCommand);  // konwersja z char na int i zwraca, tak samo jak HELLO, tylko "przetwarza"
+  sCmd.setDefaultHandler(unrecognized);      // co jesli nie ma takiej komendy]
+  
+  sCmd.addCommand("setFreq", setFrequency);
+  
+  
+  Serial.println("Ready");
+}
+
+void loop() {
+  sCmd.readSerial();     // Przetwarzanie, to wszystko co dzieje sie w petli
+}
+
+
+void LED_on() {
+  Serial.println("LED on");
+  digitalWrite(arduinoLED, HIGH);
+}
+
+void LED_off() {
+  Serial.println("LED off");
+  digitalWrite(arduinoLED, LOW);
+}
+
+void sayHello() {
+  char *arg;
+  arg = sCmd.next();    // Przyklad z buforowaniem, w ten sposob mozemy przesylac argumenty
+  if (arg != NULL) {    
+    Serial.print("Hello ");
+    Serial.println(arg);
+  }
+  else {
+    Serial.println("Hello, whoever you are");
+  }
+}
+
+
+void processCommand() {
+  int aNumber;
+  char *arg;
+
+  Serial.println("We're in processCommand");
+  arg = sCmd.next();
+  if (arg != NULL) {
+    aNumber = atoi(arg);    // Konwertuje char na int
+    Serial.print("First argument was: ");
+    Serial.println(aNumber);
+  }
+  else {
+    Serial.println("No arguments");
+  }
+
+  arg = sCmd.next();
+  if (arg != NULL) {
+    aNumber = atol(arg);
+    Serial.print("Second argument was: ");
+    Serial.println(aNumber);
+  }
+  else {
+    Serial.println("No second argument");
+  }
+}
+
+
+
+void setFrequency() {
+  int aNumber;
+  char *arg;
+
+  Serial.println("We're in setFrequency");
+  arg = sCmd.next();
+  if (arg != NULL) {
+    aNumber = atoi(arg);    // Konwertuje char na int
+    Serial.print("First argument was: ");
+    Serial.println(aNumber);
+    setPwmFrequency(freqPIN, aNumber);
+    
+  }
+  else {
+    Serial.println("No arguments");
+  }
+
+  arg = sCmd.next();
+  if (arg != NULL) {
+    aNumber = atol(arg);
+    Serial.print("Second argument was: ");
+    Serial.println(aNumber);
+    analogWrite(freqPIN, aNumber);
+    
+    
+  }
+  else {
+    Serial.println("No second argument");
+  }
+}
+
+
+// nie rozpoznano komendy
+void unrecognized(const char *command) {
+  Serial.println("What?");
+}
+
+
+
+/**
+ * Divides a given PWM pin frequency by a divisor.
+ * 
+ * The resulting frequency is equal to the base frequency divided by
+ * the given divisor:
+ *   - Base frequencies:
+ *      o The base frequency for pins 3, 9, 10, and 11 is 31250 Hz.
+ *      o The base frequency for pins 5 and 6 is 62500 Hz.
+ *   - Divisors:
+ *      o The divisors available on pins 5, 6, 9 and 10 are: 1, 8, 64,
+ *        256, and 1024.
+ *      o The divisors available on pins 3 and 11 are: 1, 8, 32, 64,
+ *        128, 256, and 1024.
+ * 
+ * PWM frequencies are tied together in pairs of pins. If one in a
+ * pair is changed, the other is also changed to match:
+ *   - Pins 5 and 6 are paired on timer0
+ *   - Pins 9 and 10 are paired on timer1
+ *   - Pins 3 and 11 are paired on timer2
+ * 
+ * Note that this function will have side effects on anything else
+ * that uses timers:
+ *   - Changes on pins 3, 5, 6, or 11 may cause the delay() and
+ *     millis() functions to stop working. Other timing-related
+ *     functions may also be affected.
+ *   - Changes on pins 9 or 10 will cause the Servo library to function
+ *     incorrectly.
+ * 
+ * Thanks to macegr of the Arduino forums for his documentation of the
+ * PWM frequency divisors. His post can be viewed at:
+ *   http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1235060559/0#4
+ */
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x7; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
+
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Aby to odpalic:
+
+Najpierw wlaczamy srodowisko Arduino.
+Potem Sketch => Import Library => Ladujemy katalog SerialCommand
+Potem otwieramy plik ino, i voila
+
