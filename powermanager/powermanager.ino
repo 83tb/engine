211781d9d8428a22193a7558c2f387c18cff621a
kuba <kuba@kucharski.it>
2014-02-24 23:18:57 +0100

    Revert "separator"
    This reverts commit 36b4371a359a1ae5e6f32e897ccd6b4496f492db.

--- powermanager/powermanager.ino
+++ powermanager/powermanager.ino
@@ -164,17 +164,16 @@ void sir_func() {
 
 void sir(int address,int registry,int packet) {
 
-   Serial.println("----------------------------");
-   
+ 
    Wire.beginTransmission(address);
-   Serial.print("Adres urzadzenia: ");
+   Serial.print("Adres urzadzenia: 0x");
    Serial.print(address, HEX);
-   Serial.print("    Adres rejestru: ");
+   Serial.print("    Adres rejestru: 0x");
    Serial.println(registry, HEX);
    
    Wire.write(registry); 
    
-   Serial.print("Wysylamy pakiet: ");
+   Serial.print("Wysylamy pakiet: 0x");
    Serial.println(packet, HEX);
    Wire.write(packet);    
    
@@ -186,7 +185,7 @@ void sir(int address,int registry,int packet) {
   { 
     char c = Wire.read(); // receive a byte as character
     Serial.print(c, BIN);
-	Serial.print(" | ");
+	Serial.print(" | 0x");
     Serial.println(c, HEX);
 
   }
@@ -198,14 +197,12 @@ void sir(int address,int registry,int packet) {
 
 
 void sio(int address,int registry) {
-   
-   Serial.println("----------------------------");
-   
+
   
    Wire.beginTransmission(address);
-   Serial.print("Adres urzadzenia: ");
+   Serial.print("Adres urzadzenia: 0x");
    Serial.print(address, HEX);
-   Serial.print("   Adres rejestru: ");
+   Serial.print("   Adres rejestru: 0x");
    Serial.println(registry, HEX);
    
    Wire.write(registry); 
@@ -219,7 +216,7 @@ void sio(int address,int registry) {
   { 
     char c = Wire.read(); 
     Serial.print(c, BIN);
-	Serial.print(" | ");
+	Serial.print(" | 0x");
     Serial.println(c, HEX);
 	
      
