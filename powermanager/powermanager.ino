36b4371a359a1ae5e6f32e897ccd6b4496f492db
kuba <kuba@kucharski.it>
2014-02-24 23:17:47 +0100

    separator

--- powermanager/powermanager.ino
+++ powermanager/powermanager.ino
@@ -164,16 +164,17 @@ void sir_func() {
 
 void sir(int address,int registry,int packet) {
 
- 
+   Serial.println("----------------------------");
+   
    Wire.beginTransmission(address);
-   Serial.print("Adres urzadzenia: 0x");
+   Serial.print("Adres urzadzenia: ");
    Serial.print(address, HEX);
-   Serial.print("    Adres rejestru: 0x");
+   Serial.print("    Adres rejestru: ");
    Serial.println(registry, HEX);
    
    Wire.write(registry); 
    
-   Serial.print("Wysylamy pakiet: 0x");
+   Serial.print("Wysylamy pakiet: ");
    Serial.println(packet, HEX);
    Wire.write(packet);    
    
@@ -185,7 +186,7 @@ void sir(int address,int registry,int packet) {
   { 
     char c = Wire.read(); // receive a byte as character
     Serial.print(c, BIN);
-	Serial.print(" | 0x");
+	Serial.print(" | ");
     Serial.println(c, HEX);
 
   }
@@ -197,12 +198,14 @@ void sir(int address,int registry,int packet) {
 
 
 void sio(int address,int registry) {
-
+   
+   Serial.println("----------------------------");
+   
   
    Wire.beginTransmission(address);
-   Serial.print("Adres urzadzenia: 0x");
+   Serial.print("Adres urzadzenia: ");
    Serial.print(address, HEX);
-   Serial.print("   Adres rejestru: 0x");
+   Serial.print("   Adres rejestru: ");
    Serial.println(registry, HEX);
    
    Wire.write(registry); 
@@ -216,7 +219,7 @@ void sio(int address,int registry) {
   { 
     char c = Wire.read(); 
     Serial.print(c, BIN);
-	Serial.print(" | 0x");
+	Serial.print(" | ");
     Serial.println(c, HEX);
 	
      
